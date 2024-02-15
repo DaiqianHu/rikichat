a30c619ca55fc89fe2ba9d392a16945cd9ef3ef4
Daiqian Hu (from Dev Box) <daih@microsoft.com>
2024-02-15 13:32:29 +0000

    adjust the app version format.

--- frontend/generate-version.js
+++ frontend/generate-version.js
@@ -2,8 +2,22 @@ import { writeFileSync } from 'fs';
 import { join } from 'path';
 
 const versionFilePath = join(process.cwd(), 'version.ts');
-const timestamp = new Date().toISOString();
 
-const content = `export const VERSION_TIMESTAMP = "v${timestamp}";\n`;
+const options = { 
+    year: 'numeric', 
+    month: '2-digit', 
+    day: '2-digit', 
+    hour: '2-digit', 
+    minute: '2-digit', 
+    second: '2-digit', 
+    hour12: false, 
+    timeZone: 'Asia/Chongqing'
+};
+
+const timestampParts = new Intl.DateTimeFormat('en-US', options).formatToParts(new Date());
+
+const formattedTimestamp = `${timestampParts[4].value}-${timestampParts[0].value}-${timestampParts[2].value}_${timestampParts[6].value}.${timestampParts[8].value}.${timestampParts[10].value}`;
+
+const content = `export const VERSION_TIMESTAMP = "v${formattedTimestamp}";\n`;
 
 writeFileSync(versionFilePath, content);
