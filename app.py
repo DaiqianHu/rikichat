d0f5fb636987f9d532a69be9d07a98e5356e134f
Daiqian Hu (from Dev Box) <daih@microsoft.com>
2024-02-18 07:46:07 +0000

    logout user if there is no authencated session.

--- app.py
+++ app.py
@@ -24,7 +24,14 @@ app = Flask(__name__, static_folder="static")
 # Static Files
 @app.route("/")
 def index():
-    return app.send_static_file("index.html")
+    try:
+        authenticated_user = get_authenticated_user_details(request_headers=request.headers)
+        user_id = authenticated_user['user_principal_id']
+
+        if DEBUG_LOGGING: logging.debug(f"Authenticated user: {user_id}")
+        return app.send_static_file("index.html")
+    except:
+        logout()
 
 @app.route("/favicon.ico")
 def favicon():
